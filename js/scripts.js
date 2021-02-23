e296529ddc76d59770da3d7055b4cebbc36873b4
SLauchli <77997175+SLauchli@users.noreply.github.com>
2021-02-23 11:49:22 -0700

    Adding JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
 console.log("Hi! Welcome to my portfolio. Enjoy your visit :)")
+
+var myNavToggle = $('#hamburger');
+myNavToggle.click(function() {
+  var closed = true;
+
+  if (closed) {
+    $("#toggle-links").removeClass("hidden");
+    closed = false;
+  } else {
+    $("#toggle-links").addClass("hidden");
+    closed = true;
+  }
+});
+
+
+
+
+
+//   if (x.className === 'nav-container') {
+//   x.className += 'responsive';
+// } else {
+//   x.className = 'nav-container borderxwidth';
+// }
+// }
